6b39e9af3f5e0b3ff26c683b992e72c988a25353
redf195 <redf195>
2020-08-14 22:01:42 -0400

    Built

--- www/store-ios.js
+++ www/store-ios.js
@@ -651,7 +651,7 @@ store.Product.prototype.verify = function() {
             }));
             var dataTransaction = getData(data, 'transaction');
             if (dataTransaction) {
-                that.transaction = Object.assign(that.transaction || {}, dataTransaction);
+                that.transaction = Object.assign({}, that.transaction || {}, dataTransaction);
                 store._extractTransactionFields(that);
                 that.trigger("updated");
             }
@@ -3822,7 +3822,7 @@ function storekitError(errorCode, errorText, options) {
 
     // when loading failed, trigger "error" for each of
     // the registered products.
-    if (errorCode === storekit.ERR_LOAD) {
+    if (errorCode === store.ERR_LOAD) {
         for (i = 0; i < store.products.length; ++i) {
             p = store.products[i];
             p.trigger("error", [new store.Error({
@@ -3835,7 +3835,7 @@ function storekitError(errorCode, errorText, options) {
     // a purchase was cancelled by the user:
     // - trigger the "cancelled" event
     // - set the product back to its original state
-    if (errorCode === storekit.ERR_PAYMENT_CANCELLED) {
+    if (errorCode === store.ERR_PAYMENT_CANCELLED) {
         p = store.get(options.productId);
         if (p) {
             p.trigger("cancelled");
